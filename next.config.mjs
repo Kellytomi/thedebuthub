1854acdc008593816c020afc4750358675343113
Etoma-etoto Odi <kelvinetoma95@gmail.com>
2025-09-25 01:08:56 +0100

    chore: update Content Security Policy and add new dependencies
    - Modified Content Security Policy in next.config.mjs to differentiate between production and development environments.
    - Added @sanity/code-input and @sanity/vision packages to package.json and package-lock.json for enhanced functionality.
    - Updated package-lock.json with new dependencies and their respective versions.

--- next.config.mjs
+++ next.config.mjs
@@ -45,7 +45,9 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live https://va.vercel-scripts.com https://core.sanity-cdn.com https://www.youtube.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https: blob:; media-src 'self' https:; connect-src 'self' https://vitals.vercel-insights.com https://api.spotify.com https://accounts.spotify.com https://*.sanity.io https://*.api.sanity.io;",
+            value: process.env.NODE_ENV === 'production' 
+              ? "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://vercel.live https://va.vercel-scripts.com https://core.sanity-cdn.com https://www.youtube.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https: blob:; media-src 'self' https:; connect-src 'self' https://vitals.vercel-insights.com https://api.spotify.com https://accounts.spotify.com https://*.sanity.io https://*.api.sanity.io;"
+              : "default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob:; script-src 'self' 'unsafe-eval' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline' https:; font-src 'self' 'unsafe-inline' https:; img-src 'self' data: https: blob:; media-src 'self' https:; connect-src 'self' https:;",
           },
         ],
       },
